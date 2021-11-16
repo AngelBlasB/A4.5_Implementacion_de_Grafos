00dd0b9e48eb54b89be7cb85f2311b35da067614
Lesli <Lesli@LAPTOP-G4BQ0J6E>
2021-11-15 19:10:03 -0600

    Se agrego clase Ejecutable Grafo

---
+++ src/Grafo/Grafo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grafo;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Lesli
+ */
+public class Grafo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame ventana=new JFrame("Grafo");
+        ventana.add(new Lienzo());
+        ventana.setSize(600,600);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+        
+        
+    }
+    
+}//FIN MAIN
+
