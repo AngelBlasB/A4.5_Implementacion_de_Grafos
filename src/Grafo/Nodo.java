26f6c476661ccf200a4bfd6efe5991818cd97cb0
lilia <lilia@192.168.1.75>
2021-11-16 18:18:45 -0600

    Cambios realizados en la clase Nodo con relación al video 3

--- src/Grafo/Nodo.java
+++ src/Grafo/Nodo.java
@@ -7,20 +7,19 @@ package Grafo;
 
 import java.awt.Graphics;
 
-/**
- *
- * @author ADMIN
- */
 public class Nodo {
     private int x,y;
+    private String nombre;
     public static final int d=60;
 
-    public Nodo(int x, int y) {
+    public Nodo(int x, int y, String nombre) {
         this.x = x;
         this.y = y;
+        this.nombre = nombre;
     }
     public void pintar(Graphics g){
         g.drawOval(this.x-d/2,this.y-d/2,d,d);
+        g.drawString(nombre, x, y);
 }
 
     public int getX() {
@@ -38,5 +37,4 @@ public class Nodo {
     public void setY(int y) {
         this.y = y;
     }
-}//FIN CLASS
-
+}
